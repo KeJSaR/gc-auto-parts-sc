99ac237f6f749c30cd69f460fba33303a9fbaeda
Constantine Verenich <verenich@gmail.com>
2017-11-03 21:29:33 +0300

    Refactor Web

--- csc/Web/index.php
+++ csc/Web/index.php
@@ -4,14 +4,14 @@ namespace SCL\Web;
 
 error_reporting(E_ALL);
 
-define('SCL_SAFETY_CONST', true);
+define("SCL_SAFETY_CONST", true);
 
-define('SCL_DS', DIRECTORY_SEPARATOR);
-define('SCL_ROOT_DIR', dirname(__DIR__) . SCL_DS);
+define("SCL_DS", DIRECTORY_SEPARATOR);
+define("SCL_ROOT_DIR", dirname(__DIR__) . SCL_DS);
 
-require_once SCL_ROOT_DIR . 'Config' . SCL_DS . 'settings.php';
+require_once SCL_ROOT_DIR . "Config" . SCL_DS . "settings.php";
 
-require_once SCL_LIB_DIR . 'Autoloader.php';
+require_once SCL_LIB_DIR . "Autoloader.php";
 $loader = new \SCL\lib\Autoloader();
 $loader->register();
 
