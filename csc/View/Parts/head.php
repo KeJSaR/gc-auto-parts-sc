6104d5e948f295a6c067cc7ec64a3ad415623955
Stijn Veren <verenich@gmail.com>
2018-09-05 16:39:04 +0300

    Remove unused css

--- csc/View/Parts/head.php
+++ csc/View/Parts/head.php
@@ -1,13 +1,4 @@
 <meta charset="utf-8">
 <link rel="stylesheet" href="css/normalize.css">
-<link rel="stylesheet" href="css/lightbox.css">
-
-<link rel="stylesheet" href="css/jquery-ui.css">
-<link rel="stylesheet" href="css/jquery-ui.structure.css">
-<link rel="stylesheet" href="css/jquery-ui.theme.css">
-
 <link href="https://fonts.googleapis.com/css?family=Roboto:400,400i,900&amp;subset=cyrillic" rel="stylesheet">
-
 <link rel="stylesheet" href="css/custom.css?1900">
-
-<link rel="stylesheet" href="css/select2.css">
