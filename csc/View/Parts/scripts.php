49925a7c1758d9d662d803767d06a3a7b8863392
Stijn Veren <verenich@gmail.com>
2018-09-05 16:38:20 +0300

    Remove unused javascript

--- csc/View/Parts/scripts.php
+++ csc/View/Parts/scripts.php
@@ -1,6 +1,2 @@
 <script src="js/jquery.js"></script>
-<script src="js/jquery-ui.min.js"></script>
-<script src="js/datepicker-ru.js"></script>
-<script src="js/lightbox.js"></script>
-<script src="js/select2.js"></script>
-<script src="js/custom.js?1800"></script>
+<script src="js/custom.js?1900"></script>
